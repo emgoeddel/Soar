b1fdf6c513df81cf6815185aa2d1f943726f4873
Lizzie Goeddel <mamantov@umich.edu>
2021-03-05 10:58:02 -0500

    Moves planning problems into their own file to make working on motor easier.

---
+++ Core/SVS/src/planning_problem.h
@@ -0,0 +1,42 @@
+#ifndef PLANNING_PROBLEM_H
+#define PLANNING_PROBLEM_H
+
+#ifdef ENABLE_ROS
+
+#include <ompl/geometric/SimpleSetup.h>
+#include <ompl/base/spaces/RealVectorStateSpace.h>
+
+#include "collision.h"
+#include "robot_model.h"
+#include "motor_state.h"
+#include "mat.h"
+
+/*
+ * planning_problem class
+ *
+ * Holds the search space and collision info for a particular planning
+ * query and runs the search through an OMPL SimpleSetup with FCL collision
+ *
+ */
+
+class planning_problem {
+public:
+    planning_problem(int qid, motor_query q, motor_state* msp, robot_model* m);
+    void find_one();
+
+private:
+    trajectory path_to_trajectory(ompl::geometric::PathGeometric& geom);
+
+    int query_id;
+    motor_query query;
+
+    std::shared_ptr<robot_model> model;
+    std::shared_ptr<motor_state> ms;
+    std::string joint_group;
+
+    ompl::geometric::SimpleSetup* ompl_ss;
+    collision_checker* cc;
+};
+
+#endif
+#endif
