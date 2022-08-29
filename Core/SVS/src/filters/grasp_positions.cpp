f9972f05751cbd88ae94d62d8a3813fd587ef6cb
Lizzie Goeddel <mamantov@umich.edu>
2022-08-29 16:57:52 -0400

    Fixes indentation in the grasp filter file.

--- Core/SVS/src/filters/grasp_positions.cpp
+++ Core/SVS/src/filters/grasp_positions.cpp
@@ -31,62 +31,62 @@
 ////// filter grasp_first //////
 class grasp_first_filter : public map_filter<vec6> {
 public:
-	grasp_first_filter(Symbol *root,
-                           soar_interface *si,
-                           scene *scn,
-                           filter_input *input) : map_filter<vec6>(root, si, input),
-                                                  scn(scn)
+    grasp_first_filter(Symbol *root,
+                       soar_interface *si,
+                       scene *scn,
+                       filter_input *input) : map_filter<vec6>(root, si, input),
+        scn(scn)
 	{}
 
-	bool compute(const filter_params *p, vec6& out) {
-		sgnode *a;
+    bool compute(const filter_params *p, vec6& out) {
+        sgnode *a;
 
-		if (!get_filter_param(this, p, "a", a)) {
-                    set_status("expecting parameter a");
-                    return false;
-		}
+        if (!get_filter_param(this, p, "a", a)) {
+            set_status("expecting parameter a");
+            return false;
+        }
 
-                std::string f;
-                if (!get_filter_param(this, p, "frame", f)) {
-                    set_status("expecting frame parameter");
-                    return false;
-		}
+        std::string f;
+        if (!get_filter_param(this, p, "frame", f)) {
+            set_status("expecting frame parameter");
+            return false;
+        }
 
-                if (!scn->node_has_grasp(a->get_id())) {
-                    set_status("a is not graspable");
-                    return false;
-                }
+        if (!scn->node_has_grasp(a->get_id())) {
+            set_status("a is not graspable");
+            return false;
+        }
 
-                std::vector<std::pair<transform3, transform3> > grasp_pairs = scn->node_grasps(a->get_id());
-                if (grasp_pairs.empty()) {
-                    set_status("a is not graspable");
-                    return false;
-                }
+        std::vector<std::pair<transform3, transform3> > grasp_pairs = scn->node_grasps(a->get_id());
+        if (grasp_pairs.empty()) {
+            set_status("a is not graspable");
+            return false;
+        }
 
-                vec6 pos;
+        vec6 pos;
 
-                if (f == "object") {
-                    grasp_pairs[0].first.xyzrpy(pos);
-                } else if (f == "world") {
-                    transform3 wt = a->get_world_trans()*grasp_pairs[0].first;
-                    wt.xyzrpy(pos);
-                } else if (f == "self") {
-                    transform3 b = scn->get_self_root()->get_world_trans();
-                    transform3 o = a->get_world_trans()*grasp_pairs[0].first;
+        if (f == "object") {
+            grasp_pairs[0].first.xyzrpy(pos);
+        } else if (f == "world") {
+            transform3 wt = a->get_world_trans()*grasp_pairs[0].first;
+            wt.xyzrpy(pos);
+        } else if (f == "self") {
+            transform3 b = scn->get_self_root()->get_world_trans();
+            transform3 o = a->get_world_trans()*grasp_pairs[0].first;
 
-                    transform3 st = b.inv()*o;
-                    st.xyzrpy(pos);
-                } else {
-                    set_status("frame parameter invalid");
-                    return false;
-                }
+            transform3 st = b.inv()*o;
+            st.xyzrpy(pos);
+        } else {
+            set_status("frame parameter invalid");
+            return false;
+        }
 
-                out = pos;
-                return true;
-	}
+        out = pos;
+        return true;
+    }
 
 private:
-	scene *scn;
+    scene *scn;
 };
 
 filter* make_grasp_first_filter(Symbol* root, soar_interface* si, scene* scn, filter_input* input)
